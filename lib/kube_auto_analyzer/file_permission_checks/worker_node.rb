a3a18765883990dba344fc86bdbd0848799eb372
Rory McCune <rory.mccune@nccgroup.com>
2017-05-17 16:34:19 +0100

    Basic working file check

--- lib/kube_auto_analyzer/file_permission_checks/worker_node.rb
+++ lib/kube_auto_analyzer/file_permission_checks/worker_node.rb
@@ -1,18 +1,23 @@
 module KubeAutoAnalyzer
 
-  def check_worker_etc
+  def self.check_worker_etc
     pod = Kubeclient::Resource.new
     pod.metadata = {}
     pod.metadata.name = "kube-auto-analyzer-file-check"
     pod.metadata.namespace = "default"
     pod.spec = {}
+    pod.spec.restartPolicy = "Never"
     pod.spec.containers = {}
-    pod.spec.containers [{name: "kubeautoanalyzerfiletest", image: "raesene/kube-auto-analyzer-agent:latest"}]
+    pod.spec.containers = [{name: "kubeautoanalyzerfiletest", image: "raesene/kube-auto-analyzer-agent:latest"}]
     pod.spec.volumes = [{name: 'etck8s', hostPath: {path: '/etc/kubernetes'}}]
     pod.spec.containers[0].volumeMounts = [{mountPath: '/hostetck8s', name: 'etck8s'}]
     pod.spec.containers[0].args = ["/hostetck8s"]
     @client.create_pod(pod)
-    sleep(5) until @client.get_pod("kube-auto-analyzer-file-check","default")['status']['containerStatuses'][0]['state']['terminated']['reason'] == "Completed"
+    begin
+      sleep(5) until @client.get_pod("kube-auto-analyzer-file-check","default")['status']['containerStatuses'][0]['state']['terminated']['reason'] == "Completed"
+    rescue
+      retry
+    end
     results = @client.get_pod_log("kube-auto-analyzer-file-check","default")
     puts results
   end
